6eb3fa17b979d733ac64387dfe975f079043295e
Furuidev <jzafaa@gmail.com>
2023-01-07 08:17:43 -0700

    Added Django todolistt

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+https://realpython.com/django-todo-lists/
+
+C:\> python -m venv venv
+C:\> venv\Scripts\activate.bat
+python manage.py runserver
+py -m pip install Django
---
+++ todo_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from todo_app.models import ToDoItem, ToDoList
+
+
+
+
+# Register your models here.
+admin.site.register(ToDoItem)
+admin.site.register(ToDoList)
